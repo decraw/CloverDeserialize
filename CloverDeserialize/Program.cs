baad96e85d1c87d50f8e55546f02c71c26edc507
duane <duane@crawhome.com>
2017-02-12 10:24:48 -0800

    Iniitial Add

---
+++ CloverDeserialize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonstr = System.IO.File.ReadAllText("Clover.json");
+            var webhook = Newtonsoft.Json.JsonConvert.DeserializeObject<WebHookBody>(jsonstr);
+            var flattened = (from merchDict in webhook.merchants
+                             from chgLog in merchDict.Value
+                             where !merchDict.Value.Any(chgLog2 => chgLog2.objectId == chgLog.objectId && chgLog2.ts > chgLog.ts)
+                             select new { merchant = merchDict.Key, change = chgLog }).ToList();
+
+            var grouped = flattened.GroupBy(f => f.merchant).ToDictionary(f => f.Key, f=> f.Select(ch => ch.change).ToList());
+            var webHookDeDuped = new WebHookBody { appId = webhook.appId, merchants = grouped };
+            var merch = webHookDeDuped.Merchants.ToList();
+        }
+    }
+}
---
+++ CloverDeserialize/WebHookBody.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class WebHookBody
+    {
+        public string appId { get; set; }
+        public Dictionary<string, List<ChangeLog>> merchants { get; set; }
+
+        public IEnumerable<Merchant> Merchants
+        {
+            get
+            {
+                return from x in merchants
+                        select new Merchant { Name = x.Key, Changes = x.Value };
+            }
+        }
+    }
+    public class ChangeLog
+    {
+        public string objectId { get; set; }
+        public string type { get; set; }
+        public long ts { get; set; }
+
+        public string ObjectType
+        {
+            get
+            {
+                return objectId.Split(':').First();
+            }
+
+        }
+        public string ObjectKey
+        {
+            get
+            {
+                return objectId.Split(':').Last();
+            }
+        }
+    }
+    public class Merchant
+    {
+        public string Name { get; set; }
+        public List<ChangeLog> Changes { get; set; }
+    }
+}
